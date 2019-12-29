f80d2450049e45b21ec2d062d41e4f23d9bf2c8a
Rinkesh Patel <rinkeshpatel22@gmail.com>
2019-12-29 17:52:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,13 @@ app.use(bodyParser.json());
 app.all('*', (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    if ('OPTIONS' == req.method) {
+        res.sendStatus(200);
+    }
+    else {
+        next();
+    }
 });
 
 //import routes
